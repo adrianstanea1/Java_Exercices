bb78158dc848487a9346d14461c80f1e954c534d
unknown <Stanea.Ni.Adrian@student.utcluj.ro>
2022-02-08 11:42:29 +0200

    Minor update

---
+++ Section_5/Control_Flow/src/academy/learnprogramming/Main.java
@@ -0,0 +1,25 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code
+
+        int value = 5;
+
+        switch (value) {
+            case 1:
+                System.out.println("Your value was 1");
+                break;
+            case 2:
+                System.out.println("Your value was 2");
+                break;
+            case 3:
+                System.out.println("Your value was 3");
+                break;
+            default:
+                System.out.println("Your value is not covered");
+
+        }
+    }
+}
